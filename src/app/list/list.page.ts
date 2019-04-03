7864cff2a3359c7b9959cff39c067aaa4707d2ea
Prafful Jagtap <prafful.amexs@gmail.com>
2019-04-03 19:15:10 +0530

    done

--- src/app/list/list.page.ts
+++ src/app/list/list.page.ts
@@ -9,11 +9,42 @@ import { UserService } from '../api/user.service'
 export class ListPage implements OnInit {
   message: string;
   rowdata
+  purchase = [];
   constructor(public navCtrl: NavController, private UserService: UserService) {
-    this.UserService.alldata('/product/product')
+    // var id = 1
+    var data = {
+      id: 1
+    }
+    this.UserService.postAllData('/order/purchase', data)
       .subscribe(response => {
         this.rowdata = response.json()
-        // console.log(this.rowdata, 'res');
+        console.log(this.rowdata, 'res');
+        var d = this.rowdata.Product
+        // console.log(d);
+
+        this.rowdata.map(user => {
+          var t = user.Product;
+          // console.log(t, 'res');
+          t.map(t => {
+            // console.log(t.Product_id);
+            var data = {
+              order_id: user.order_id,
+              order_date: user.order_date,
+              product_id: t.Product_id,
+              product_name: t.Product_name,
+              product_price: t.Product_price,
+              product_url: t.Product_url,
+              product_url2: t.Product_url2
+            }
+            // console.log(data);
+            this.purchase.push(data)
+
+
+
+          })
+        })
+        console.log(this.purchase);
+
 
       });
   }
